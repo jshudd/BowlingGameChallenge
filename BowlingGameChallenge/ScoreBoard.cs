8b29a02d4a6a1df073110c45253b028b1bcf7d9c
Jeremy Huddleston <jshudd@gmail.com>
2022-04-10 15:11:31 -0500

    added CalculateScore()

--- BowlingGameChallenge/ScoreBoard.cs
+++ BowlingGameChallenge/ScoreBoard.cs
@@ -32,6 +32,96 @@ namespace BowlingGameChallenge
 
         }
 
-        
+        public static void CalculateScore(Player p)
+        {
+            //calculate score for frames 1 - 8            
+            for (int i = 0; i < 8; i++)
+            {
+                //3 Strikes in a row!!!
+                if (p.ScoreArray[1, i] == 10 && p.ScoreArray[1, i + 1] == 10 && p.ScoreArray[1, i + 2] == 10)
+                {
+                    p.Score += 30;
+                }
+                //2 Strikes in a row
+                else if (p.ScoreArray[1, i] == 10 && p.ScoreArray[1, i + 1] == 10 && p.ScoreArray[1, i + 2] != 10)
+                {
+                    p.Score += 20 + p.ScoreArray[1, i + 2];
+                }
+                //1 Strike
+                else if (p.ScoreArray[1, i] == 10 && p.ScoreArray[1, i + 1] != 10)
+                {
+                    p.Score += 10 + p.ScoreArray[1, i + 1] + p.ScoreArray[2, i + 1];
+                }
+                //!strike; spare == true
+                else if (p.ScoreArray[1, i] != 10 && p.ScoreArray[1, i] + p.ScoreArray[2, i] == 10)
+                {
+                    p.Score += 10 + p.ScoreArray[1, i + 1];
+                }
+
+                //!strike or !spare
+                else if (p.ScoreArray[1, i] != 10 && p.ScoreArray[1, i] + p.ScoreArray[2, i] != 10)
+                {
+                    p.Score += p.ScoreArray[1, i] + p.ScoreArray[2, i];
+                }
+            }
+
+            //calculate 9th frame
+            //3 Strikes in a row!!!
+            if (p.ScoreArray[1, 8] == 10 && p.ScoreArray[1, 9] == 10 && p.ScoreArray[2, 9] == 10)
+            {
+                p.Score += 30;
+            }
+            //2 Strikes in a row
+            else if (p.ScoreArray[1, 8] == 10 && p.ScoreArray[1, 9] == 10 && p.ScoreArray[2, 9] != 10)
+            {
+                p.Score += 20 + p.ScoreArray[2, 9];
+            }
+            //1 Strike
+            else if (p.ScoreArray[1, 8] == 10 && p.ScoreArray[1, 9] != 10)
+            {
+                p.Score += 10 + p.ScoreArray[1, 9] + p.ScoreArray[2, 9];
+            }
+            //!strike; spare == true
+            else if (p.ScoreArray[1, 8] != 10 && p.ScoreArray[1, 8] + p.ScoreArray[2, 8] == 10)
+            {
+                p.Score += 10 + p.ScoreArray[1, 9];
+            }
+
+            //!strike or !spare
+            else if (p.ScoreArray[1, 8] != 10 && p.ScoreArray[1, 8] + p.ScoreArray[2, 8] != 10)
+            {
+                p.Score += p.ScoreArray[1, 8] + p.ScoreArray[2, 8];
+            }
+
+            //calculate 10th frame for final score
+            //3 Strikes in a row!!!
+            if (p.ScoreArray[1, 9] == 10 && p.ScoreArray[2, 9] == 10 && p.ScoreArray[3, 9] == 10)
+            {
+                p.Score += 30;
+            }
+            //2 Strikes in a row
+            if (p.ScoreArray[1, 9] == 10 && p.ScoreArray[2, 9] == 10 && p.ScoreArray[3, 9] != 10)
+            {
+                p.Score += 20 + p.ScoreArray[3, 9];
+            }
+            //1 Strike
+            if (p.ScoreArray[1, 9] == 10 && p.ScoreArray[2, 9] != 10)
+            {
+                p.Score += 10 + p.ScoreArray[2, 9] + p.ScoreArray[3, 9];
+            }
+            //!strike; spare == true
+            if (p.ScoreArray[1, 9] != 10 && p.ScoreArray[1, 9] + p.ScoreArray[2, 9] == 10 || p.ScoreArray[2, 9] + p.ScoreArray[3, 9] == 10)
+            {
+                p.Score += 10 + p.ScoreArray[3, 9];
+            }
+            //!strike or !spare
+            if (p.ScoreArray[1, 9] != 10 && p.ScoreArray[1, 9] + p.ScoreArray[2, 9] != 10)
+            {
+                p.Score += p.ScoreArray[1, 9] + p.ScoreArray[2, 9];
+            }
+
+            Console.WriteLine($"Final Score: {p.Score}");
+
+        }
     }
 }
