7c2494880850f5a9566b83f92c1f207f74c91d96
Jeremy Huddleston <jshudd@gmail.com>
2022-04-10 14:32:41 -0500

    added properties to Player class

--- BowlingGameChallenge/Player.cs
+++ BowlingGameChallenge/Player.cs
@@ -8,6 +8,23 @@ namespace BowlingGameChallenge
 {
     public class Player
     {
+        public Player()
+        {
+        }
 
+        public string Name { get; set; }
+        public int CurrentFrame { get; set; } = 1;
+        public int NumberOfPins { get; set; } = 10;
+
+        //public bool Strike { get; set; } = false;
+        //public bool Spare { get; set; } = false;
+        public int Score { get; set; } = 0;
+        public int[,] ScoreArray { get; set; } = new int[4, 10]
+        {
+            { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
+        };
     }
 }
