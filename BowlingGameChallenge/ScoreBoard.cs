a4a770350a63928069d726a7b847ea2dff31f8b5
Jeremy Huddleston <jshudd@gmail.com>
2022-04-10 15:10:49 -0500

    added PrintScoreBoard()

--- BowlingGameChallenge/ScoreBoard.cs
+++ BowlingGameChallenge/ScoreBoard.cs
@@ -6,8 +6,32 @@ using System.Threading.Tasks;
 
 namespace BowlingGameChallenge
 {
-    public class ScoreBoard
+    public static class ScoreBoard
     {
 
+        public static void PrintScoreBoard(Player p)
+        {
+            var counter = 1;
+            Console.Write("Frame    ");
+            Console.Write("Roll 1    ");
+            Console.Write("Roll 2    ");
+            Console.Write("Roll 3\n");
+            do
+            {
+                Console.Write($"{counter}         ");
+                Console.Write($"{p.ScoreArray[1, counter - 1]}         ");
+                Console.Write($"{p.ScoreArray[2, counter - 1]}         \n");
+                counter++;
+
+            } while (counter < 10);
+
+            Console.Write($"{counter}        ");
+            Console.Write($"{p.ScoreArray[1, counter - 1]}         ");
+            Console.Write($"{p.ScoreArray[2, counter - 1]}         ");
+            Console.Write($"{p.ScoreArray[3, counter - 1]}         \n");
+
+        }
+
+        
     }
 }
